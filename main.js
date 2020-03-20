43c398e81d75f4843db133e5d9d87c5aaf39b4c5
esteban0829 <esteban0829@naver.com>
2020-03-20 09:15:50 +0900

    css file added

--- main.js
+++ main.js
@@ -12,19 +12,23 @@ http.createServer(function (req, res) {
   var pathname = url.parse(req.url).pathname;
   var querydata = url.parse(req.url).query;
 
-  console.log('--------------------');
+
   console.log(`_url : ${_url}`);
   console.log(`pathanme : ${pathname}`);
   console.log(`querydata : ${querydata}`);
-  // console.log(querystring.parse(querydata));
-  // console.log(querystring.parse(querydata).id);
-  console.log('--------------------');
 
   if(pathname === '/'){
     fs.readFile('./html/index.html', function(err, html){
       if(err) throw err;
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(html);
+    });
+
+  if(pathname === '/master.css'){
+    fs.readFile('./css/master.css', function(err, css){
+      if(err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(css);
     });
 
   }else if(pathname === '/apply_class'){
