7ac5b76f10e730579ffbe88796041b230c36b900
Carlos Flores <carlosfloresalex@gmail.com>
2020-06-15 22:32:28 -0400

    switched ajax request, using fetch api instead. renamed profile-card to project-card

--- front-end/src/js/project-card-create.js
+++ front-end/src/js/project-card-create.js
@@ -1,46 +1,52 @@
-var request = new XMLHttpRequest();
+// Event listener triggers when the window loads
+window.addEventListener('load', () => {
+    // project cards will load if(error) then a sample card is loaded instead
+    try { loadProjectCards() } catch{ loadExampleProjectCard() };
+});
 
-request.open('POST', '/customer_list', true);
+//fetches data with a POST request
+async function loadProjectCards() {
+    const request = await fetch('/customer_list', { method: 'POST' })
+    const data = await request.json();
+    makeProjectCards(data);
+};
 
-//The script will try to get the data for the profile cards
-// If there is an error a example card will be created with a error message
-try{
-    request.onload = function initialCards() {
+function makeProjectCards(customerData) {
+    const projectContainer = document.querySelector('.project-container');
+    const profileCardHtml = `<div class="project-card"></div>`
+    let key = Object.keys(customerData);
+    let keyLength = key.length;
+    for (let i = 0; i < keyLength; i++) {
+        const projectCard = document.createRange().createContextualFragment(profileCardHtml);
+        projectContainer.appendChild(projectCard);
 
-        var customer = JSON.parse(this.response);
-    
-        const projectContainer = document.querySelector('.profile-container');
-        const profileCardHtml = `<div class="profile-card"></div>`
-    
-        var key = Object.keys(customer);
-    
-        for (var i=0;i<key.length;i++){
-            const profileCard = document.createRange().createContextualFragment(profileCardHtml);
-            const profileHtml = `
-            <div class="profile-icon"></div>
+        const projectFragment = document.createRange().createContextualFragment(projectCardElement(customerData, i));
+        projectContainer.children[i].appendChild(projectFragment);
+    }
+    function projectCardElement(customer, i) {
+        // Returns the html as a string
+        projectCardHTML = `
+            <div class="project-icon"></div>
             <div class="profile-description">
-            <h1>${customer[key[i]].first_name + " " + customer[key[i]].last_name}</h1>
-            <p>${customer[key[i]].email}</p>
-            <p>${customer[key[i]].phone_number}</p>
-            </div>
-            `
-            projectContainer.appendChild(profileCard);
-            const profileFragment = document.createRange().createContextualFragment(profileHtml);
-            projectContainer.children[i].appendChild(profileFragment);
-        }
-    };
-    request.send();
-}catch{
+                <h1>${customer[key[i]].first_name + " " + customer[key[i]].last_name}</h1>
+                <p>${customer[key[i]].email}</p>
+                <p>${customer[key[i]].phone_number}</p>
+            </div>`
+        return projectCardHTML;
+    }
+}
+
+function loadExampleProjectCard() {
     const container = document.querySelector('.profile-container');
     profileCardErr = `
-    <div class="profile-card">
-    <div class="profile-icon"></div>
-    <div class="profile-description">
-    <h1>Error loading Profiles</h1>
-    <p>eaxample@mail.com</p>
-    <p>example</p>
-    </div>
-    </div>`
+        <div class="project-card">
+        <div class="project-icon"></div>
+        <div class="project-description">
+        <h1>Error loading project-cards</h1>
+        <p>eaxample@mail.com</p>
+        <p>example</p>
+        </div>
+        </div>`
     const profileCardErrFrag = document.createRange().createContextualFragment(profileCardErr);
     container.appendChild(profileCardErrFrag);
 }
